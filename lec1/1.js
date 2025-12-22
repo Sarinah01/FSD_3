d3356fec92e33722fa59d400554d55285fe74964
Sarinah <zaidchudiwala@gmail.com>
2025-12-22 13:30:07 +0530

    Add files via upload

---
+++ lec1/1.js
@@ -0,0 +1,42 @@
+document.writeln("Write Data!")
+document.writeln("Hello <br>")
+document.writeln("Testing")
+
+// function and data types:
+//var keyword::
+//local- func
+//global out
+// //undefined if printing without tht!
+// var a =10
+// function my(){
+//    var b = 20; //local
+   
+//     console.log(a,b)
+// }
+// console.log(a)
+// // console.log(b)
+// my()
+// console.log(c)
+// var c=5 //undefined
+// console.log(c)
+// console.log(y)
+// let y =3
+// console.log(y)
+
+//let keyword:
+// let a =10
+// function my(){
+//    let b = 20; //local
+   
+//     console.log(a,b)
+// }
+// console.log(a)
+// // console.log(b)--reference error
+// my()
+// console.log(c)
+// let c=5 //undefined
+// // initialization error
+// console.log(c)
+// console.log(y)
+// let y =3
+// console.log(y)
